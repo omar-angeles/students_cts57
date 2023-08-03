5f8d8dc461b759462258f6236c74f6a69d2c8c18
Omar Zavaleta <221271030@alumnos.utn.edu.mx>
2023-08-02 22:57:48 -0600

    tarjetas en home

--- ctrl/procesamientoExcel.php
+++ ctrl/procesamientoExcel.php
@@ -31,81 +31,106 @@ $numeroLetra = Coordinate::columnIndexFromString($letra);
 
 
 for($indiceFila = 2; $indiceFila<=$numeroFilas; $indiceFila++){
+
+
     
     $curp = $hojaActual->getCellByColumnAndRow(11, $indiceFila);
-    $noControl = $hojaActual->getCellByColumnAndRow(12, $indiceFila);
-    $apellidoPaterno = $hojaActual->getCellByColumnAndRow(3, $indiceFila);
-    $apellidoMaterno = $hojaActual->getCellByColumnAndRow(4, $indiceFila);
-    $nombres = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
-    $sexoAlumno = $hojaActual->getCellByColumnAndRow(6, $indiceFila);
-    $fotoAlumno = $hojaActual->getCellByColumnAndRow(7, $indiceFila);
-    $fechaNacimientoAlumno = $hojaActual->getCellByColumnAndRow(8, $indiceFila);
-    $edadAlumno = $hojaActual->getCellByColumnAndRow(9, $indiceFila);
-    $ultimoSemestreAlumno = $hojaActual->getCellByColumnAndRow(13, $indiceFila);
-    $turnoAlumno = $hojaActual->getCellByColumnAndRow(14, $indiceFila);
-    $grupoAlumno = $hojaActual->getCellByColumnAndRow(15, $indiceFila);
-    $especialidadAlumno = $hojaActual->getCellByColumnAndRow(16, $indiceFila);
-    $becaBenito = $hojaActual->getCellByColumnAndRow(17, $indiceFila);
-    $trabajaAlumno = $hojaActual->getCellByColumnAndRow(18, $indiceFila);
-    $tipoSecundaria = $hojaActual->getCellByColumnAndRow(19, $indiceFila);
-    $hablaLenguaIndigena = $hojaActual->getCellByColumnAndRow(21, $indiceFila);
-    $domicilioAlumno = $hojaActual->getCellByColumnAndRow(22, $indiceFila);
+    $curp = trim($curp);
 
-    //Estos tienen datos nulos en la base de datos
-    $localidad = $hojaActual->getCellByColumnAndRow(24, $indiceFila);
-    $entidadFederativa = $hojaActual->getCellByColumnAndRow(24, $indiceFila);
+    // empieza if de curp vacio
+    // if($curp == ""){
+    //     break;
+    // }else{
 
-    $codigoPostal = $hojaActual->getCellByColumnAndRow(26, $indiceFila);
-    $noExterior = $hojaActual->getCellByColumnAndRow(27, $indiceFila);
-    $noInterior = $hojaActual->getCellByColumnAndRow(28, $indiceFila);
-    $descripcionCasa = $hojaActual->getCellByColumnAndRow(29, $indiceFila);
+        $noControl = $hojaActual->getCellByColumnAndRow(12, $indiceFila);
+        $apellidoPaterno = $hojaActual->getCellByColumnAndRow(3, $indiceFila);
+        $apellidoMaterno = $hojaActual->getCellByColumnAndRow(4, $indiceFila);
+        $nombres = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
+        $sexoAlumno = $hojaActual->getCellByColumnAndRow(6, $indiceFila);
+        $fotoAlumno = $hojaActual->getCellByColumnAndRow(7, $indiceFila);
+        $fechaNacimientoAlumno = $hojaActual->getCellByColumnAndRow(8, $indiceFila);
+        $edadAlumno = $hojaActual->getCellByColumnAndRow(9, $indiceFila);
+        $ultimoSemestreAlumno = $hojaActual->getCellByColumnAndRow(13, $indiceFila);
+        $turnoAlumno = $hojaActual->getCellByColumnAndRow(14, $indiceFila);
+        $grupoAlumno = $hojaActual->getCellByColumnAndRow(15, $indiceFila);
+        $especialidadAlumno = $hojaActual->getCellByColumnAndRow(16, $indiceFila);
+        $becaBenito = $hojaActual->getCellByColumnAndRow(17, $indiceFila);
+        $trabajaAlumno = $hojaActual->getCellByColumnAndRow(18, $indiceFila);
+        $tipoSecundaria = $hojaActual->getCellByColumnAndRow(19, $indiceFila);
+        $hablaLenguaIndigena = $hojaActual->getCellByColumnAndRow(21, $indiceFila);
+        $domicilioAlumno = $hojaActual->getCellByColumnAndRow(22, $indiceFila);
 
-    $viveConPadres = $hojaActual->getCellByColumnAndRow(59, $indiceFila);
-    $conQuienVive = $hojaActual->getCellByColumnAndRow(60, $indiceFila);
-    $estatura = $hojaActual->getCellByColumnAndRow(61, $indiceFila);
-    $peso = $hojaActual->getCellByColumnAndRow(62, $indiceFila);
-    $servicioSeguro = $hojaActual->getCellByColumnAndRow(63, $indiceFila);
-    $alumnoMedicado = $hojaActual->getCellByColumnAndRow(64, $indiceFila);
-    $nombreEnfermedad = $hojaActual->getCellByColumnAndRow(65, $indiceFila);
-    $alumnoSobresaliente = $hojaActual->getCellByColumnAndRow(68, $indiceFila);
+        //Estos tienen datos nulos en la base de datos
+        $localidad = $hojaActual->getCellByColumnAndRow(24, $indiceFila);
+        $entidadFederativa = $hojaActual->getCellByColumnAndRow(24, $indiceFila);
 
-    $tipoSobresaliente = $hojaActual->getCellByColumnAndRow(69, $indiceFila);
-    $alumnoConTratamientoPsicologico = $hojaActual->getCellByColumnAndRow(70, $indiceFila);
-    $documentoAlumnoPsicologico = $hojaActual->getCellByColumnAndRow(71, $indiceFila);
+        $codigoPostal = $hojaActual->getCellByColumnAndRow(26, $indiceFila);
+        $noExterior = $hojaActual->getCellByColumnAndRow(27, $indiceFila);
+        $noInterior = $hojaActual->getCellByColumnAndRow(28, $indiceFila);
+        $descripcionCasa = $hojaActual->getCellByColumnAndRow(29, $indiceFila);
 
-    $tipoTransporte = $hojaActual->getCellByColumnAndRow(72, $indiceFila);
-    $tiempoTransporte = $hojaActual->getCellByColumnAndRow(73, $indiceFila);
-    $totalTransporteSemanal = $hojaActual->getCellByColumnAndRow(74, $indiceFila);
-    $nombreUniversidadFutura = $hojaActual->getCellByColumnAndRow(75, $indiceFila);
+        $viveConPadres = $hojaActual->getCellByColumnAndRow(59, $indiceFila);
+        $conQuienVive = $hojaActual->getCellByColumnAndRow(60, $indiceFila);
+        $estatura = $hojaActual->getCellByColumnAndRow(61, $indiceFila);
+        $peso = $hojaActual->getCellByColumnAndRow(62, $indiceFila);
+        $servicioSeguro = $hojaActual->getCellByColumnAndRow(63, $indiceFila);
+        $alumnoMedicado = $hojaActual->getCellByColumnAndRow(64, $indiceFila);
+        $nombreEnfermedad = $hojaActual->getCellByColumnAndRow(65, $indiceFila);
+        $alumnoSobresaliente = $hojaActual->getCellByColumnAndRow(68, $indiceFila);
 
-    $gastoUtiles = $hojaActual->getCellByColumnAndRow(79, $indiceFila);
-    $gastoUniformes = $hojaActual->getCellByColumnAndRow(80, $indiceFila);
-    $internetEnCasa = $hojaActual->getCellByColumnAndRow(81, $indiceFila);
-    $dispositivoDisponibles = $hojaActual->getCellByColumnAndRow(82, $indiceFila);
-    $reglamentoAlumno = $hojaActual->getCellByColumnAndRow(83, $indiceFila);
-    $reglamentoTutor = $hojaActual->getCellByColumnAndRow(84, $indiceFila);
+        $tipoSobresaliente = $hojaActual->getCellByColumnAndRow(69, $indiceFila);
+        $alumnoConTratamientoPsicologico = $hojaActual->getCellByColumnAndRow(70, $indiceFila);
+        $documentoAlumnoPsicologico = $hojaActual->getCellByColumnAndRow(71, $indiceFila);
 
-    $firmaAlumno = $hojaActual->getCellByColumnAndRow(85, $indiceFila);
-    $firmaTutor = $hojaActual->getCellByColumnAndRow(86, $indiceFila);
-    $estatusNSS = $hojaActual->getCellByColumnAndRow(87, $indiceFila);
-    $numeroNSSAlumno = $hojaActual->getCellByColumnAndRow(88, $indiceFila);
-    $localidadSeguroSocial = $hojaActual->getCellByColumnAndRow(89, $indiceFila);
-    $numeroCasaAlumno = $hojaActual->getCellByColumnAndRow(90, $indiceFila);
-    $numeroCelularAlumno = $hojaActual->getCellByColumnAndRow(10, $indiceFila);
+        $tipoTransporte = $hojaActual->getCellByColumnAndRow(72, $indiceFila);
+        $tiempoTransporte = $hojaActual->getCellByColumnAndRow(73, $indiceFila);
+        $totalTransporteSemanal = $hojaActual->getCellByColumnAndRow(74, $indiceFila);
+        $nombreUniversidadFutura = $hojaActual->getCellByColumnAndRow(75, $indiceFila);
 
-    //Estos tienen datos nulos en la base de datos
-    // $idCarrera = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
-    // $idPariente = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
-    // $idGrupo = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
-    // $idDiscapacidad = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
+        $gastoUtiles = $hojaActual->getCellByColumnAndRow(79, $indiceFila);
+        $gastoUniformes = $hojaActual->getCellByColumnAndRow(80, $indiceFila);
+        $internetEnCasa = $hojaActual->getCellByColumnAndRow(81, $indiceFila);
+        $dispositivoDisponibles = $hojaActual->getCellByColumnAndRow(82, $indiceFila);
+        $reglamentoAlumno = $hojaActual->getCellByColumnAndRow(83, $indiceFila);
+        $reglamentoTutor = $hojaActual->getCellByColumnAndRow(84, $indiceFila);
 
-    $sql = "INSERT INTO alumnos (curp, noControl, apellidoPaterno, apellidoMaterno, nombres, sexoAlumno, fotoAlumno, fechaNacimientoAlumno, edadAlumno, ultimoSemestreAlumno, turnoAlumno, grupoAlumno, especialidadAlumno, becaBenito, trabajaAlumno, tipoSecundaria, hablaLenguaIndigena, domicilioAlumno, localidad, entidadFederativa, codigoPostal, noExterior, noInterior, descripcionCasa, viveConPadres, conQuienVive, estatura, peso, servicioSeguro, alumnoMedicado, nombreEnfermedad, alumnoSobresaliente, tipoDeSobreSaliente, alumnoConTratamientoPsicologico, documentoAlumnoPsicologico, tipoTransporte, tiempoTransporte, totalTransporteSemanal, nombreUniversidadFutura, gastoUtiles, gastoUniformes, internetEnCasa, dispositivoDisponibles, reglamentoAlumno, reglamentoTutor, firmaAlumno, firmaTutor, estatusNSS, numeroNSSAlumno, localidadSeguroSocial, numeroCasaAlumno, numeroCelularAlumno) VALUES ('$curp', '$noControl', '$apellidoPaterno', '$apellidoMaterno', '$nombres', '$sexoAlumno', '$fotoAlumno', '$fechaNacimientoAlumno', '$edadAlumno', '$ultimoSemestreAlumno', '$turnoAlumno', '$grupoAlumno', '$especialidadAlumno', '$becaBenito', '$trabajaAlumno', '$tipoSecundaria', '$hablaLenguaIndigena', '$domicilioAlumno', '$localidad', '$entidadFederativa', '$codigoPostal', '$noExterior', '$noInterior', '$descripcionCasa', '$viveConPadres', '$conQuienVive', '$estatura', '$peso', '$servicioSeguro', '$alumnoMedicado', '$nombreEnfermedad', '$alumnoSobresaliente', '$tipoSobresaliente', '$alumnoConTratamientoPsicologico', '$documentoAlumnoPsicologico', '$tipoTransporte', '$tiempoTransporte', '$totalTransporteSemanal', '$nombreUniversidadFutura', '$gastoUtiles', '$gastoUniformes', '$internetEnCasa', '$dispositivoDisponibles', '$reglamentoAlumno', '$reglamentoTutor', '$firmaAlumno', '$firmaTutor', '$estatusNSS', '$numeroNSSAlumno', '$localidadSeguroSocial', '$numeroCasaAlumno', '$numeroCelularAlumno')";
+        $firmaAlumno = $hojaActual->getCellByColumnAndRow(85, $indiceFila);
+        $firmaTutor = $hojaActual->getCellByColumnAndRow(86, $indiceFila);
+        $estatusNSS = $hojaActual->getCellByColumnAndRow(87, $indiceFila);
+        $numeroNSSAlumno = $hojaActual->getCellByColumnAndRow(88, $indiceFila);
+        $localidadSeguroSocial = $hojaActual->getCellByColumnAndRow(89, $indiceFila);
+        $numeroCasaAlumno = $hojaActual->getCellByColumnAndRow(90, $indiceFila);
+        $numeroCelularAlumno = $hojaActual->getCellByColumnAndRow(10, $indiceFila);
+
+        //Estos tienen datos nulos en la base de datos
+        // $idCarrera = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
+        // $idPariente = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
+        // $idGrupo = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
+        // $idDiscapacidad = $hojaActual->getCellByColumnAndRow(5, $indiceFila);
+
+        $sql = "INSERT INTO alumnos (curp, noControl, apellidoPaterno, apellidoMaterno, nombres, sexoAlumno, fotoAlumno, fechaNacimientoAlumno, edadAlumno, ultimoSemestreAlumno, turnoAlumno, grupoAlumno, especialidadAlumno, becaBenito, trabajaAlumno, tipoSecundaria, hablaLenguaIndigena, domicilioAlumno, localidad, entidadFederativa, codigoPostal, noExterior, noInterior, descripcionCasa, viveConPadres, conQuienVive, estatura, peso, servicioSeguro, alumnoMedicado, nombreEnfermedad, alumnoSobresaliente, tipoDeSobreSaliente, alumnoConTratamientoPsicologico, documentoAlumnoPsicologico, tipoTransporte, tiempoTransporte, totalTransporteSemanal, nombreUniversidadFutura, gastoUtiles, gastoUniformes, internetEnCasa, dispositivoDisponibles, reglamentoAlumno, reglamentoTutor, firmaAlumno, firmaTutor, estatusNSS, numeroNSSAlumno, localidadSeguroSocial, numeroCasaAlumno, numeroCelularAlumno) VALUES ('$curp', '$noControl', '$apellidoPaterno', '$apellidoMaterno', '$nombres', '$sexoAlumno', '$fotoAlumno', '$fechaNacimientoAlumno', '$edadAlumno', '$ultimoSemestreAlumno', '$turnoAlumno', '$grupoAlumno', '$especialidadAlumno', '$becaBenito', '$trabajaAlumno', '$tipoSecundaria', '$hablaLenguaIndigena', '$domicilioAlumno', '$localidad', '$entidadFederativa', '$codigoPostal', '$noExterior', '$noInterior', '$descripcionCasa', '$viveConPadres', '$conQuienVive', '$estatura', '$peso', '$servicioSeguro', '$alumnoMedicado', '$nombreEnfermedad', '$alumnoSobresaliente', '$tipoSobresaliente', '$alumnoConTratamientoPsicologico', '$documentoAlumnoPsicologico', '$tipoTransporte', '$tiempoTransporte', '$totalTransporteSemanal', '$nombreUniversidadFutura', '$gastoUtiles', '$gastoUniformes', '$internetEnCasa', '$dispositivoDisponibles', '$reglamentoAlumno', '$reglamentoTutor', '$firmaAlumno', '$firmaTutor', '$estatusNSS', '$numeroNSSAlumno', '$localidadSeguroSocial', '$numeroCasaAlumno', '$numeroCelularAlumno')";
+
+        $mysqli->query($sql);
 
 
-    
-    $mysqli->query($sql);
 
+        try{
+
+        $curp = ($curp);
+        $curpNo = strlen($curp);
+        if (($curpNo < 17) or ($curpNo > 18)) {
+            echo '<div class="alert alert-danger" role="alert">Error al registrar al alumn@: ' . $nombres . ' ' . $apellidoPaterno . ' ' . $apellidoMaterno . ' debido a que el CURP no contiene el numero de caracteres apropiados.</div>';
+        }
+
+        }
+        catch(Exception $e){
+            die(print_r($e->getMessage()));
+        }
+
+    // } termina if de curp vacio
+
+        
 }
 echo 'Carga completa';
-header("Location: http://localhost/proyectos/students_cts57/tablesPreview.php?nume=1");
+// header("Location: http://localhost/proyectos/students_cts57/tablesPreview.php?nume=1");
             
--- home.php
+++ home.php
@@ -2,37 +2,70 @@
 
 <body class="table-responsive">
   <br>
-<div class="row inicial">
-  <div id="example" class="col-lg-6">
-    <div class="card border-success">
-      <div class="card-header bg-success text-white">
-        Cargar información por documento de excel.
+  <div class="row inicial ">
+
+    <div id="example" class="col-lg-6">
+      <div class="card border-success">
+        <div class="card-header bg-success text-white">
+          Cargar información por documento de excel.
+        </div>
+        <div class="card-body">
+          <p class="card-text">Con esta opción, usted puede cargar la información mediante una plantilla de Excel.</p>
+          <form action="ctrl/archivosTempExcel/temp.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="archivo" required>
+            <center><br><button type="submit" id="excel" class="btn btn-outline-success"><i class="far fa-file-excel"></i> Subir archivo</button></center>
+          </form>
+        </div>
       </div>
-      <div class="card-body">
-        <p class="card-text">Con esta opción, usted puede cargar la información mediante una plantilla de Excel.</p>
-        <form action="ctrl/archivosTempExcel/temp.php" method="POST" enctype="multipart/form-data">
-          <input type="file" name="archivo" required>
-          <center><br><button type="submit" id="excel" class="btn btn-outline-success"><i class="far fa-file-excel"></i> Subir archivo</button></center>
-        </form>
-      </div>
+      <br>
     </div>
+
+    <div id="example" class="col-lg-6">
+      <div class="card border-info">
+        <div class="card-header bg-info text-white">
+          Listado de alumnos en base de datos
+        </div>
+        <div class="card-body">
+          <p class="card-text">Con esta opción, usted puede ver el listado de alumnos que se encuentra en la base de datos actualmente</p>
+          <center><a href="tablesPreview.php?nume=1" class="btn btn-outline-info">Listado de alumnos actuales</a></center>
+        </div>
+      </div>
+      <br>
+    </div>
+
   </div>
   
-  <div id="example" class="col-lg-6">
-    
-    <div class="card border-info">
-      <div class="card-header bg-info text-white">
-        Listado de alumnos en base de datos
+  <div class="row inicial">
+    <div id="example" class="col-lg-6">
+      <div class="card border-warning">
+        <div class="card-header bg-warning text-white">
+          Justificantes de alumnos
+        </div>
+        <div class="card-body">
+          <p class="card-text">Con esta opción, usted puede realizar justificantes</p>
+          <center><a href="#" class="btn btn-outline-warning">Justificantes</a></center>
+        </div>
       </div>
-      <div class="card-body">
-        <p class="card-text">Con esta opción, usted puede ver el listado de alumnos que se encuentra en la base de datos actualmente</p>
-          <center><a href="tablesPreview.php?nume=1" class="btn btn-outline-info">Listado de alumnos actuales</a></center>
+      <br>
+    </div>
+
+    <div id="example" class="col-lg-6">
+      <div class="card border-danger">
+        <div class="card-header bg-danger text-white">
+          Reportes de alumnos
+        </div>
+        <div class="card-body">
+          <p class="card-text">Con esta opción, usted puede realizar Reportes</p>
+          <center><a href="#" class="btn btn-outline-danger">Reportes</a></center>
+        </div>
       </div>
     </div>
+    
   </div>
-</div>
-<br><br><br><br><br><br>
+  <br><br><br><br><br><br>
 </body>
+
+
 
 
 <?php include("partials/footer.php"); ?>
